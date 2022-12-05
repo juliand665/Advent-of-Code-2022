720e313b9b829fbc791f8ba3676a81d7def56803
Julian Dunskus <julian.dunskus@gmail.com>
2022-12-05 07:11:10 +0100

    day 5

---
+++ Day 05/main.swift
@@ -0,0 +1,33 @@
+import Foundation
+import AoC_Helpers
+import SimpleParser
+import Algorithms
+
+typealias Crate = Character
+
+struct Move {
+	var count, source, target: Int
+	
+	func apply(to stacks: inout [[Crate]], shouldFlip: Bool) {
+		let temp = stacks[source - 1].suffix(count)
+		stacks[source - 1].removeLast(count)
+		if shouldFlip { // temp.reversed() and temp have different types so no ?:
+			stacks[target - 1].append(contentsOf: temp.reversed())
+		} else {
+			stacks[target - 1].append(contentsOf: temp)
+		}
+	}
+}
+
+let (rawGrid, rawMoves) = input().lines().split(whereSeparator: \.isEmpty).extract()
+
+let grid = Matrix(rawGrid.dropLast().map {
+	$0.dropFirst().chunks(ofCount: 4).map { $0.first! }
+})
+let initial = grid.transposed().rows.map {
+	$0.drop { $0 == " " }.reversed() as Array
+}
+let moves = rawMoves.map { $0.ints().splat(Move.init) }
+
+print(String(moves.reduce(into: initial) { $1.apply(to: &$0, shouldFlip: true) }.map(\.last!)))
+print(String(moves.reduce(into: initial) { $1.apply(to: &$0, shouldFlip: false) }.map(\.last!)))
